10420e7bf0ba244196b85f52054a053eb1e0b548
RWojcikowski <radoslaw.chicqen@gmail.com>
2022-09-09 17:39:22 +0200

    add count clicks and  reset button

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,37 @@ span.onclick = function() {
       popup.style.display = "none";
     }
   };
+
+  // button count click //
+  let add = document.querySelector("#btn");
+
+  add.addEventListener("click", function () {
+    let number = document.querySelector("#number");
+    let result = Number(number.innerText) + 1;
+    number.innerText = result;
+  });
+
+
+// button reset clicks //
+function reset() {
+  let number = 0;
+
+  document.getElementById("number").innerHTML = number; 
+}
+
+//button show if > 5//
+
+let btnReset = document.getElementById('btn');
+btnReset.addEventListener('click', ()=>{
+  let element = document.getElementById('reset');
+	if (element.style.display != 'flex'){
+    	element.style.display = 'flex';
+    }
+  else{
+    element.style.display = 'none'
+  }
+})
+
+
+
+
